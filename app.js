7814761698a51b8c555768cdeb1ce1afbafdc962
Taylor Arnold <taylorj.arnold2013@hotmail.com>
2021-01-04 18:23:15 -0800

    code update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // creating the ids to the dropdown   
 d3.json('samples.json').then((data)=>{
     var id=data.names;
-    console.log(data.metadata);
+    // console.log(data.metadata);
     var select=d3.selectAll('#selDataset');
     Object.entries(id).forEach(([i,v])=>{
         select.append('option').text(v);
